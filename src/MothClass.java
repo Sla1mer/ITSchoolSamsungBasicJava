a5fa69e5298b777a6d8f7f8ebfec14a77bd465a3
Назар Садик <Slameryt4@gmail.com>
2019-10-17 19:51:10 +0200

    Task 1.5: zadanie 4

---
+++ src/MothClass.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MothClass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        switch (a){
+            case (1):
+                System.out.println(31);
+                break;
+            case (2):
+                System.out.println(28);
+                break;
+            case (3):
+                System.out.println(31);
+                break;
+            case (4):
+                System.out.println(30);
+                break;
+            case (5):
+                System.out.println(31);
+                break;
+            case (6):
+                System.out.println(30);
+                break;
+            case (7):
+                System.out.println(31);
+                break;
+            case (8):
+                System.out.println(31);
+                break;
+            case (9):
+                System.out.println(30);
+                break;
+            case (10):
+                System.out.println(31);
+                break;
+            case (11):
+                System.out.println(30);
+                break;
+            case (12):
+                System.out.println(31);
+                break;
+            default:
+                System.out.println(0);
+        }
+    }
+}
