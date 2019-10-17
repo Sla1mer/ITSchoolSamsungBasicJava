54d09c3a6c65e31e8bbbfc398df5b3ad0df78997
Назар Садик <Slameryt4@gmail.com>
2019-10-17 19:12:09 +0200

    Additional task 1.2: zadanie 1

---
+++ src/LastNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LastNumber {
+    public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                int a = in.nextInt();
+                int b = a % 10;
+                System.out.println(b);
+    }
+}
