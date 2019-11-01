0cf41b6559ccb5cf56c3e0a2c178389341fe2ba5
Назар Садик <Slameryt4@gmail.com>
2019-11-01 14:11:38 +0200

    HomeTask: Цыклы "Автобусная остановка", не дорешал, не могу понять как сделать правильно.

--- src/CikliClass3.java
+++ src/CikliClass3.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class CikliClass3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arrBridge = new int[in.nextInt()];
+        int h = 437;
+        for (int i = 0; i<arrBridge.length; i++){
+            arrBridge[i] = in.nextInt();
+        }
+        for (int i = 0; i<arrBridge.length; i++){
+            if(arrBridge[i] < h){
+                System.out.println("Crash" + " " + arrBridge[i]);
+            }else {
+                System.out.println("No Crash");
+            }
+        }
+    }
 }
